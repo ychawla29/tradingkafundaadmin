64ea8a8bcceb6664fab904a6671c444afcb9538a
Yogesh Chawla <yogesh.chawla@niyantras.com>
2021-02-08 01:44:16 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "23f41235be6608c4f176f91645159d2d",
-"/": "23f41235be6608c4f176f91645159d2d",
-"main.dart.js": "1509fef309dd48e4802aab554ad05483",
+"index.html": "ddd2e0b05a2d9e1cd7b5c000502a4187",
+"/": "ddd2e0b05a2d9e1cd7b5c000502a4187",
+"main.dart.js": "46bb66501e5036e66ee330c1f483a336",
 "manifest.json": "89da6edc9a9b270b5938a7beaeaecf73",
 "version.json": "7b171daeb99a61922784784988424556"
 };
